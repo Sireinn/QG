898b16a7e6a256f6143f4782859bc1aa389a3c31
Sireinn <2532494160@qq.com>
2026-02-04 00:34:16 +0800

    �Զ��ύ: 2026-02-04 00:34:15

--- 一键提交.bat
+++ 一键提交.bat
@@ -1,9 +1,13 @@
 @echo off
 chcp 65001 > nul
 echo 正在提交更改到Git...
+
+:: 使用PowerShell获取标准格式的时间，避免乱码
+for /f "delims=" %%a in ('powershell -Command "Get-Date -Format 'yyyy-MM-dd HH:mm:ss'"') do set "datetime=%%a"
+
 cd /d "D:\obsidian\知识库\QG寒假训练营"
 git add .
-git commit -m "自动提交: %date:~0,4%-%date:~5,2%-%date:~8,2% %time:~0,8%"
+git commit -m "自动提交: %datetime%"
 echo 正在推送到GitHub...
 git push
 echo.
